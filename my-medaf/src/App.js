0367324f984b673296a16aa294438fc200afa488
mamenkiwi <mamenkiwi@gmail.com>
2022-10-09 18:58:52 +0000

    cd

---
+++ my-medaf/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//$ git remote add origin https://github.com/{username}/{repo-name}.git
